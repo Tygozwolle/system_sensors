a28dd43309aabbf2d8e536b02bee35f16549ca1a
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-01-29 17:02:21 +0100

    fix Trailing whitespace (#43)

--- src/system_sensors.py
+++ src/system_sensors.py
@@ -246,7 +246,7 @@ if __name__ == '__main__':
     deviceNameDisplay = settings['devicename']
     deviceManufacturer = "RPI Foundation" if "rasp" in OS_DATA["ID"] else OS_DATA['NAME']
     deviceModel = get_host_model()
-    
+
 
     mqttClient = mqtt.Client(client_id=settings['client_id'])
     mqttClient.on_connect = on_connect                      #attach function to callback
