66f2d9777ac6a7a7c0b1cc5112b8d7ddfeedec03
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-17 14:54:10 +0200

    small change

--- src/sensors.py
+++ src/sensors.py
@@ -369,7 +369,7 @@ def get_fan_speed():
         return None
 def get_load_GPU():
     try:
-        gpus = GPUtil.getGPUs()
+        gpus = GPUtil.getFirstAvailable()
         for gpu in gpus:
             load = gpu.load
             return load
@@ -378,7 +378,7 @@ def get_load_GPU():
         return None
 def get_temp_gpu():
     try:
-        gpus = GPUtil.getGPUs()
+        gpus = GPUtil.getFirstAvailable()
         for gpu in gpus:
             temp = gpu.temperature
             return temp
